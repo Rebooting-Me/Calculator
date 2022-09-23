7e3b9a67c063dae0ba352bedad74cc75d62f237b
Rebooting-Me <arsharma@ucsd.edu>
2022-09-23 00:21:51 -0700

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+let bold = document.getElementById("big-text")
+
+function one() {
+    bold.value += 1;
+}
+function two() {
+    bold.value += 2;
+}
+function three() {
+    bold.value += 3;
+}
+function four() {
+    bold.value += 4;
+}
+function five() {
+    bold.value += 5;
+}
+function six() {
+    bold.value += 6;
+}
+function seven() {
+    bold.value += 7;
+}
+function eight() {
+    bold.value += 8;
+}
+function nine() {
+    bold.value += 9;
+}
+function zero() {
+    bold.value += 0;
+}
+function dot() {
+    bold.value += ".";
+}
